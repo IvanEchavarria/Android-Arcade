2830831ca90b715ef3736e6c126dbc30ca3b428e
IvanEchavarria <x4y0e@unb.ca>
2018-04-12 19:02:04 -0400

    background no action bar

--- app/src/main/java/imanitystudios/junglemonkeys/MainMenu.java
+++ app/src/main/java/imanitystudios/junglemonkeys/MainMenu.java
@@ -1,9 +1,11 @@
 package imanitystudios.junglemonkeys;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 
@@ -16,6 +18,9 @@ public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
