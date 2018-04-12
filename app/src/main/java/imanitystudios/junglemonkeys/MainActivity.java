f41fee9180cc5414e088f701877dbadeeae4cf57
IvanEchavarria <x4y0e@unb.ca>
2018-04-12 17:28:53 -0400

    Enemy Assets and mainactivity start up

--- app/src/main/java/imanitystudios/junglemonkeys/MainActivity.java
+++ app/src/main/java/imanitystudios/junglemonkeys/MainActivity.java
@@ -1,13 +1,74 @@
+/*
+Source File:   MainActivity
+Authors & IDs: Taha Saleem     100803048\
+               Ivan Echavarria 101092562
+Creation Date: 12/04/2018
+
+
+File Description: it's our Main activity
+
+*/
+
 package imanitystudios.junglemonkeys;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
+    Rect rectToBeDrawn;
+
+    Canvas canvas;
+
+    Bitmap enemyAnimation;
+    Bitmap background;
+
+    int frameHeight = 221;
+    int frameWidth = 224;
+    int frameNumber;
+
+    int screenWidth;
+    int screenHeight;
+
+    long lastFrameTime;
+    int fps;
+    int hi;
+
+    Intent i;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size); // Gets the screen size and puts in a point type that contains x and y
+        screenWidth = size.x;
+        screenHeight = size.y;
+
+        background = BitmapFactory.decodeResource(getResources(), R.drawable.background);
     }
+
+
+
+
+
+
+
 }
